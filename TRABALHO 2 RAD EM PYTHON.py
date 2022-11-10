bd526c47c9ead330aeaa0cebf9c83f492bf69d64
Lucas Pereira <pereirapro123@gmail.com>
2022-11-09 21:02:32 -0300

    Update TRABALHO 2 RAD EM PYTHON.py

--- TRABALHO 2 RAD EM PYTHON.py
+++ TRABALHO 2 RAD EM PYTHON.py
@@ -201,3 +201,48 @@ def abrirArquivo(): # Função Abrir Arquivo
             print(Linha[8])
             print(Linha[9])
       
+
+    
+    
+        # Parte para apresentar ao usuário
+        A = tk.Label(janelaapresentar, text="Os dados foram salvos como:")
+        A.grid(row=0, column=0)
+        B = tk.Label(janelaapresentar, text="-------------")
+        B.grid(row=0, column=1)
+        C = tk.Label(janelaapresentar, text="Dados Inseridos:")
+        C.grid(row=3, column=0)
+        D = tk.Label(janelaapresentar, text="-------------")
+        D.grid(row=4, column=0)
+        E = tk.Label(janelaapresentar, text="-------------")
+        E.grid(row=5, column=0)
+        F = tk.Label(janelaapresentar, text="-------------")
+        F.grid(row=6, column=0)
+        G = tk.Label(janelaapresentar, text="-------------")
+        G.grid(row=7, column=0)
+        H = tk.Label(janelaapresentar, text="-------------")
+        H.grid(row=8, column=0)
+        I = tk.Label(janelaapresentar, text="-------------")
+        I.grid(row=9, column=0)
+        J = tk.Label(janelaapresentar, text="-------------")
+        J.grid(row=10, column=0)
+        K = tk.Label(janelaapresentar, text="-------------")
+        K.grid(row=11, column=0)
+        L = tk.Label(janelaapresentar, text="-------------")
+        L.grid(row=12, column=0)
+        M = tk.Label(janelaapresentar, text="-------------")
+        M.grid(row=13, column=0)
+
+        # Alterando os Valores de texto acima
+        B["text"] = NomeArquivo.get()
+        D["text"] = Linha[0].rstrip("\n")
+        E["text"] = Linha[1].rstrip("\n")
+        F["text"] = Linha[2].rstrip("\n")
+        G["text"] = Linha[3].rstrip("\n")
+        H["text"] = Linha[4].rstrip("\n")
+        I["text"] = Linha[5].rstrip("\n")
+        J["text"] = Linha[6].rstrip("\n")
+        K["text"] = Linha[7].rstrip("\n")
+        L["text"] = Linha[8].rstrip("\n")
+        M["text"] = Linha[9].rstrip("\n")
+
+        tk.Button(janelaapresentar, text="Voltar o menu",command=lambda: [janelaabrir.destroy(), janelaapresentar.destroy()]).grid(row=13, column=1)
